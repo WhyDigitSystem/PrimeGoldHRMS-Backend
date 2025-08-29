512b36e1a1f84cb28c4ef6cbf797c149911c7b68
Dhinesh <dhinesh@whydigit.in>
2025-08-29 10:45:20 +0530

    payonhand bug fixed

--- src/main/java/com/efit/hrms/repo/SalaryProcessRepo.java
+++ src/main/java/com/efit/hrms/repo/SalaryProcessRepo.java
@@ -48,15 +48,17 @@ public interface SalaryProcessRepo extends JpaRepository<SalaryProcessVO, Long> 
 //		Set<Object[]> getNetPayForSalaryProcess(BigDecimal grossPay, BigDecimal sumOfDetection);
 
 
-	@Query(value = "SELECT ROUND(((CAST(?2 AS DECIMAL) - CAST(?4 AS DECIMAL)) / CAST(?1 AS DECIMAL)) \r\n"
-			+ "             * CAST(?3 AS DECIMAL) \r\n"
-			+ "             + CAST(?5 AS DECIMAL), 2) AS payonhand\r\n"
-			+ "\r\n"
+	@Query(value = "	SELECT ROUND(((CAST(?2 AS DECIMAL(10,2)) - CAST(?4 AS DECIMAL(10,2)))\r\n"
+			+ "            / CAST(?1 AS DECIMAL(10,2))\r\n"
+			+ "        ) * CAST(?3 AS DECIMAL(10,2))\r\n"
+			+ "        + CAST(?5 AS DECIMAL(10,2)),\r\n"
+			+ "        2\r\n"
+			+ "    ) AS payOnHand "
 			+ "", 
 		       nativeQuery = true)
 	Set<Object[]> getPayOnHandsForSalaryProcess(Long totalCompanyWorkingDays, BigDecimal grossPay, BigDecimal empSalaryDays,
 			BigDecimal sumOfDetection, BigDecimal otAmount);
-	
+
 	
 
 	@Query(nativeQuery = true, value = "SELECT * \r\n"
